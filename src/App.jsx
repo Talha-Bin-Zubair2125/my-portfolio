198de650f2778d25ecf6f0f88ac4a2a46355723f
Talha-Bin-Zubair <talhazubair2125@gmail.com>
2025-11-03 15:43:54 +0500

    Updated portfolio sections and design

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import Navigation_Bar_with_Body from "./Components/Navigation_Bar_with_Body"
-import AboutMe from "./Components/AboutMe"
-import EducationalDetails from "./Components/EducationalDetails"
-import Skills from "./Components/Skills"
-import Projects from "./Components/Projects"
-import Goal from "./Components/Goal"
-import Footer from "./Components/Footer"
-function App() {
-  
+import React from "react";
+import Navigation_Bar_with_Body from "./Components/Navigation_Bar_with_Body";
+import AboutMe from "./Components/AboutMe";
+import Goal from "./Components/Goal";
+import Projects from "./Components/Projects";
+import Skills from "./Components/Skills";
+import Certifications from "./Components/Certifications"; // <-- Import here
+import EducationalDetails from "./Components/EducationalDetails";
+import Footer from "./Components/Footer";
 
+function App() {
   return (
-    <>
-      <Navigation_Bar_with_Body/>
-      <AboutMe/>
-      <EducationalDetails/>
-      <Skills/>
-      <Projects/>
-      <Goal/>
-      <Footer/>
-    </>
-  )
+    <div className="App">
+      <Navigation_Bar_with_Body />
+      <AboutMe />
+      <Goal />
+      <EducationalDetails />
+      <Projects />
+      <Skills />
+      <Certifications /> 
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
